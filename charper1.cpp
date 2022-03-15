ad6e7c11c9303125b498ad361e14d0d5c52a0149
qiqiplus <100407191+qiqiplus@users.noreply.github.com>
2022-03-15 15:45:30 +0800

    Add files via upload

---
+++ charper1.cpp
@@ -0,0 +1,22 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+///////////image //////////////
+
+void main() {
+
+	VideoCapture cap(0);
+	Mat img;
+	while (true)
+	{
+		cap.read(img);
+		imshow("Image", img);
+		waitKey(1);
+	}
+
+}
